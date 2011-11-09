55789f6760237bc8002196fe596af41bbf953c7e
nirbhay <nirbhayrane@ee.iitb.ac.in>
2011-11-09 16:23:55 +0000

    roughwork - Sequence.py updated - T flip flop created

--- roughWork/sequence.py
+++ roughWork/sequence.py
@@ -24,5 +24,24 @@ class DFlipFlop (LG):
 		if (not self.C.value) and self.prev:	# clock drop
 			self.Q.set(self.D.value)
 		self.prev = self.C.value
-		
 
+
+		
+# ================
+# T - FlipFlop
+# ================
+
+class TFlipFlop (LG):
+
+	def __init__(self, name):
+		LG.__init__(self,name)
+		self.T = Connector(self,'T')
+		self.Q = Connector(self,'Q')
+		self.C = Connector(self,'C', activates = 1)
+		self.Q.value = 0
+		self.prev = 0
+		
+	def evaluate (self):
+		if (not self.C.value) and self.prev and self.T.value:	# clock drop and T=1
+			self.Q.set(not self.Q.value)
+		self.prev = self.C.value
