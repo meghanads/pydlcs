1478efa17781303d94cee6799e516d27b7026bdc
nirbhay <nirbhayrane@ee.iitb.ac.in>
2011-11-09 16:00:19 +0000

    src - Sequence.py added

---
+++ src/sequence.py
@@ -0,0 +1,28 @@
+
+# ============================================
+# Sequential Circuits
+# ============================================
+
+
+from pydlcs import *
+
+# ================
+# D - FlipFlop
+# ================
+
+class DFlipFlop (LG):
+
+	def __init__(self, name):
+		LG.__init__(self,name)
+		self.D = Connector(self,'D')
+		self.Q = Connector(self,'Q')
+		self.C = Connector(self,'C', activates = 1)
+		self.Q.value = 0
+		self.prev = 0
+
+	def evaluate (self):
+		if (not self.C.value) and self.prev:	# clock drop
+			self.Q.set(self.D.value)
+		self.prev = self.C.value
+		
+
