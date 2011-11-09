38159c260057a749a06f7b1bd8481a2025a8bfb1
nirbhay <nirbhayrane@ee.iitb.ac.in>
2011-11-09 16:31:42 +0000

    exanples - D flip flop example updated

--- examples/expl_D_FlipFlop_plot.py
+++ examples/expl_D_FlipFlop_plot.py
@@ -5,24 +5,21 @@
 #	1. plotting working
 
 
-from sequential import *
+from sequence import *
 
-sim = SIMU('sim1',start = 0,plots = 1, debug = 1 , step =0, pannotate = 1)
+sim = SIMU('sim1',start = 0,plots = 1, debug =1, pclk = 1 , step =0, pannotate = 1)
 
 I = Istream('IN', fname = 'inp', stream =1)
 O = Ostream('OUT', stream = 1)
 
-D1 = DFlipFlop('D1')
-N = Not('N')
+DF = DFlipFlop('DF')
 
-sim.clk_out.connect([I.clk_in, N.A, O.clk_in])
-I.data_out.connect([D1.D])
-N.B.connect([D1.C])
-
-D1.Q.connect([O.data_in])
+sim.clk_out.connect([I.clk_in, DF.C, O.clk_in])
+I.data_out.connect([DF.D])
+DF.Q.connect([O.data_in])
 
 sim.addplot([I.data,O.data])
-sim.addpname(["input1","output"])
+sim.addpname(["input","output"])
 
 #sim.addplot([I.data])
 #sim.addpname(["input"])
