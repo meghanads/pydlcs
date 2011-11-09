2aab46e474fbf1efedca58961b5eb08b6f539c9f
nirbhay <nirbhayrane@ee.iitb.ac.in>
2011-11-09 20:24:49 +0000

    src - sequence.py updated - T FF class added

--- roughWork/sequence.py
+++ roughWork/sequence.py
@@ -45,3 +45,29 @@ class TFlipFlop (LG):
 		if (not self.C.value) and self.prev and self.T.value:	# clock drop and T=1
 			self.Q.set(not self.Q.value)
 		self.prev = self.C.value
+
+
+# ================
+# JK - FlipFlop
+# ================
+
+class JKFlipFlop (LG):
+
+	def __init__(self, name):
+		LG.__init__(self,name)
+		self.J = Connector(self,'J')
+		self.K = Connector(self,'K')
+		self.Q = Connector(self,'Q')
+		self.C = Connector(self,'C', activates = 1)
+		self.Q.value = 0
+		self.prev = 0
+		
+	def evaluate (self):
+		if (not self.C.value) and self.prev and self.J.value and (not self.K.value):	
+			self.Q.set(1)
+		if (not self.C.value) and self.prev and not(self.J.value) and self.K.value:	
+			self.Q.set(0)
+		if (not self.C.value) and self.prev and self.J.value and self.K.value:	
+			self.Q.set(not self.Q.value)
+		self.prev = self.C.value
+
